7bbbb6bdf3d70ee771200749375c8c72e1e75d69
masyumero <167562220+masyumero@users.noreply.github.com>
2026-08-16 15:30:16 +0900

    refactor(singularities): use MAUtils for resource location creation
    * Simplified the creation of singularities by utilizing MAUtils for resource location handling.
    * Improved code readability and maintainability.

--- src/main/java/io/github/masyumero/mekavaritia/common/registry/MASingularities.java
+++ src/main/java/io/github/masyumero/mekavaritia/common/registry/MASingularities.java
@@ -3,6 +3,7 @@ package io.github.masyumero.mekavaritia.common.registry;
 import committee.nova.mods.avaritia.core.singularity.Singularity;
 import io.github.masyumero.mekavaritia.MekanismAvaritia;
 import io.github.masyumero.mekavaritia.MekanismAvaritiaLang;
+import io.github.masyumero.mekavaritia.common.util.MAUtils;
 import mekanism.common.registries.MekanismItems;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.crafting.Ingredient;
@@ -15,8 +16,8 @@ public class MASingularities {
 
     public static final List<Singularity> SINGULARITIES = new ArrayList<>();
 
-    public static final Singularity CRYSTALLINE_ALLOY = create(new ResourceLocation(MekanismAvaritia.MODID, "crystalline_alloy"), MekanismAvaritiaLang.CRYSTALLINE_ALLOY_SINGULARITY.getTranslationKey(), new int[]{0x6fb0ac, 0x134340}, Ingredient.of(MAItems.CRYSTALLINE_ALLOY));
-    public static final Singularity ANTIMATTER_PERRET = create(new ResourceLocation(MekanismAvaritia.MODID, "antimatter_pellet"), MekanismAvaritiaLang.ANTIMATTER_PELLET_SINGULARITY.getTranslationKey(), new int[]{0x9654a6, 0xbd80a5}, Ingredient.of(MekanismItems.ANTIMATTER_PELLET));
+    public static final Singularity CRYSTALLINE_ALLOY = create(MAUtils.rl("crystalline_alloy"), MekanismAvaritiaLang.CRYSTALLINE_ALLOY_SINGULARITY.getTranslationKey(), new int[]{0x6fb0ac, 0x134340}, Ingredient.of(MAItems.CRYSTALLINE_ALLOY));
+    public static final Singularity ANTIMATTER_PERRET = create(MAUtils.rl("antimatter_pellet"), MekanismAvaritiaLang.ANTIMATTER_PELLET_SINGULARITY.getTranslationKey(), new int[]{0x9654a6, 0xbd80a5}, Ingredient.of(MekanismItems.ANTIMATTER_PELLET));
 
     private static Singularity create(ResourceLocation registryName, String displayName, int[] colors, Ingredient ingredient) {
         Singularity singularity = Singularity.create(registryName, displayName, colors, ingredient);
